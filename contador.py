43d8c4d5b52b4a6c923bcc9e5173b448c12c78f7
Franco de Simone <fsimone@grupotr.es>
2025-12-30 16:19:13 +0100

    actualizar

--- contador.py
+++ contador.py
@@ -1,14 +1,39 @@
 pares = 0
 impares = 0
 
-while True:
-    numero = int(input("Ingresa un numero (0 para salir): "))
-    if numero == 0:
-        break #Salirmos del bucle
-    if numero % 2 == 0:
-        pares += 1
-    else:
-        impares += 1
 
-print(f"Números pares: ", pares)
-print(f"Números impares: ", impares)
+# while True:
+#     numero = int(input("Ingresa un numero (0 para salir): "))
+#     if numero == 0:
+#         break #Salirmos del bucle
+#     if numero % 2 == 0:
+#         pares += 1
+#     else:
+#         impares += 1
+
+# print(f"Números pares: ", pares)
+# print(f"Números impares: ", impares)
+
+# contador = 5
+
+# while contador > 0:
+#     print("Estoy dentro del bucle... Quedan", contador, "iteraciones")
+#     contador -= 1 # Restamos 1 al contador
+
+# print("Fin del programa")
+
+# n = 3
+
+# while n > 0:
+#     print("Valor de n:", n)
+#     n -= 1
+# else:
+#     print ("El bucle termino correctamente")
+
+
+#Verificar si ambas condiciones son verdaderas
+
+# edad = 25
+# salario = 30000
+# if edad > 18 and salario > 25000:
+#     print("Cumples con los requisitos")
